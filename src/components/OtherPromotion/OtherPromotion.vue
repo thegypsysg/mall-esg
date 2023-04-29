29cd435750f3da00fc984eb8f3f8b9ce13a41076
ajiprasetyo471 <ajiprsty4713@gmail.com>
2023-04-29 10:41:05 +0700

    make other promotions section desktop and mobile

---
+++ src/components/OtherPromotion/OtherPromotion.vue
@@ -0,0 +1,227 @@
+<template>
+  <div
+    class="promotion-container mt-n10"
+    :class="{ 'other-grey': !isSmall, 'other-white': isSmall }"
+  >
+    <v-container>
+      <div v-if="!isSmall" class="d-flex justify-space-between">
+        <h1>Other Promotion Types</h1>
+        <h1>View all</h1>
+      </div>
+      <v-sheet v-if="!isSmall" class="mx-auto" elevation="0">
+        <v-slide-group v-model="model2" class="pa-4 other-slide">
+          <template #prev="{ on, attrs }">
+            <v-btn color="white" rounded icon v-bind="attrs" v-on="on">
+              <v-icon>mdi-arrow-left</v-icon>
+            </v-btn>
+          </template>
+          <template #next="{ on, attrs }">
+            <v-btn color="white" rounded icon v-bind="attrs" v-on="on">
+              <v-icon>mdi-arrow-right</v-icon>
+            </v-btn>
+          </template>
+          <v-slide-group-item
+            v-for="(item, index) in items"
+            :key="index"
+            v-slot="{ toggle }"
+            class="other-item mx-4"
+          >
+            <v-card
+              :class="['mx-2 py-10 px-12 text-left']"
+              elevation="1"
+              style="border-radius: 12px; padding: 22px"
+              @click="toggle"
+            >
+              <div
+                class="text-h6"
+                style="
+                  font-size: 20px;
+                  margin-bottom: 20px;
+                  line-height: 19.36px;
+                "
+              >
+                {{ item.title }}
+              </div>
+              <div class="d-flex">
+                <v-card
+                  v-for="(promo, i) in item.items.slice(0, 3)"
+                  :key="i"
+                  :class="['text-left', 'mr-2']"
+                  elevation="0"
+                  @click="toggle"
+                >
+                  <p class="mb-2" style="font-size: 10px; font-weight: 500">
+                    {{ promo.title }}
+                  </p>
+                  <img :src="promo.img" height="60" />
+                  <div
+                    style="
+                      font-weight: 600;
+                      font-size: 10px;
+                      line-height: 16.94px;
+                    "
+                  >
+                    <span class="text-red">{{ promo.quantity }}</span> Promos
+                  </div>
+                </v-card>
+              </div>
+              <v-btn class="other-btn"> View All Promotions </v-btn>
+            </v-card>
+          </v-slide-group-item>
+        </v-slide-group>
+      </v-sheet>
+      <div v-if="isSmall" class="px-2">
+        <v-card
+          v-for="(item, index) in displayedCardMobile"
+          :key="index"
+          elevation="0"
+          class="mb-5"
+        >
+          <div
+            class="text-h6"
+            style="font-size: 20px; margin-bottom: 20px; line-height: 19.36px"
+          >
+            Promotions {{ item.title }}
+          </div>
+          <v-slide-group class="slide-group">
+            <v-slide-group-item
+              v-for="(promo, i) in item.items"
+              :key="i"
+              class="slide-item"
+            >
+              <v-card
+                :class="['text-left', 'mr-2']"
+                elevation="0"
+                @click="toggle"
+              >
+                <p class="mb-2" style="font-size: 10px; font-weight: 500">
+                  {{ promo.title }}
+                </p>
+                <img :src="promo.img" height="60" />
+                <div
+                  style="
+                    font-weight: 600;
+                    font-size: 10px;
+                    line-height: 16.94px;
+                  "
+                >
+                  <span class="text-red">{{ promo.quantity }}</span> Promos
+                </div>
+              </v-card>
+            </v-slide-group-item>
+          </v-slide-group>
+        </v-card>
+        <v-btn
+          v-if="showViewAllButton === true"
+          class="other-btn"
+          @click="showAllCards"
+        >
+          View All Promotions
+        </v-btn>
+      </div>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "OtherPromotion",
+  props: ["items"],
+  data() {
+    return {
+      screenWidth: window.innerWidth,
+      displayedCardMobile: [],
+      showViewAllButton: true,
+      maxCardMobile: 3,
+    };
+  },
+  computed: {
+    isSmall() {
+      return this.screenWidth < 640;
+    },
+  },
+  created() {
+    window.addEventListener("resize", this.handleResize);
+  },
+  mounted() {
+    this.displayedCardMobile = this.items.slice(0, this.maxCardMobile);
+  },
+  unmounted() {
+    window.removeEventListener("resize", this.handleResize);
+  },
+  methods: {
+    showAllCards() {
+      this.displayedCardMobile = this.items;
+      this.showViewAllButton = false;
+    },
+    handleResize() {
+      this.screenWidth = window.innerWidth;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.other-grey {
+  background: #f2f2f5 !important;
+}
+
+.other-white {
+  background: #fff !important;
+}
+
+.other-grey h1 {
+  color: black !important;
+  font-weight: bolder;
+}
+
+.other-grey .other-slide {
+  background: #f2f2f5 !important;
+}
+
+.other-item {
+  margin-right: 10px !important;
+}
+
+.other-btn {
+  margin-top: 20px;
+  display: block;
+  font-weight: 400;
+  font-size: 14px;
+  color: #ef1f43;
+  border-radius: 0;
+  border: 0.5px solid #ef1f43;
+}
+
+.card-container {
+  display: flex;
+  flex-direction: column;
+}
+
+.slide-group {
+  display: flex;
+}
+
+.slide-item {
+  width: calc(
+    100% / 3
+  ); /* Ubah 3 menjadi jumlah maksimum item yang ingin ditampilkan dalam satu baris */
+  box-sizing: border-box;
+  padding: 16px;
+  text-align: left;
+}
+
+@media screen and (max-width: 960px) {
+  .slide-item {
+    width: calc(
+      100% / 2
+    ); /* Ubah 2 menjadi jumlah maksimum item yang ingin ditampilkan dalam satu baris */
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .slide-item {
+    width: 100%;
+  }
+}
+</style>
--- src/components/Promotions.vue
+++ src/components/Promotions.vue
@@ -1,39 +1,18 @@
 <template>
   <v-container>
-    <v-sheet
-      class="mx-auto mt-5 discount-sheet"
-      elevation="0"
-    >
-      <v-slide-group
-        v-model="model"
-      >
+    <v-sheet class="mx-auto mt-5 discount-sheet" elevation="0">
+      <v-slide-group v-model="model">
         <template #prev="{ on, attrs }">
-          <v-btn
-            color="white"
-            rounded
-            icon
-            v-bind="attrs"
-            v-on="on"
-          >
+          <v-btn color="white" rounded icon v-bind="attrs" v-on="on">
             <v-icon>mdi-arrow-left</v-icon>
           </v-btn>
         </template>
         <template #next="{ on, attrs }">
-          <v-btn
-            color="white"
-            rounded
-            icon
-            v-bind="attrs"
-            v-on="on"
-          >
+          <v-btn color="white" rounded icon v-bind="attrs" v-on="on">
             <v-icon>mdi-arrow-right</v-icon>
           </v-btn>
         </template>
-        <v-slide-group-item
-          v-for="n in 15"
-          :key="n"
-          v-slot="{ toggle }"
-        >
+        <v-slide-group-item v-for="n in 15" :key="n" v-slot="{ toggle }">
           <div>
             <v-card
               :class="['ma-4 pa-4']"
@@ -41,20 +20,14 @@
               variant="outlined"
               @click="toggle"
             >
-              <div class="d-flex fill-height align-center justify-space-between">
-                <img
-                  src="@/assets/images/icons/off-20.png"
-                  height="40"
-                >
-                <div class="text-h6 mb-1">
-                  20% off
-                </div>
+              <div
+                class="d-flex fill-height align-center justify-space-between"
+              >
+                <img src="@/assets/images/icons/off-20.png" height="40" />
+                <div class="text-h6 mb-1">20% off</div>
               </div>
             </v-card>
-            <div
-              class="text-center"
-              style="font-weight: 600"
-            >
+            <div class="text-center" style="font-weight: 600">
               20% Promotion
             </div>
           </div>
@@ -64,41 +37,26 @@
   </v-container>
   <div class="promotion-container">
     <v-container>
-      <div class="d-flex justify-space-between mt-5">
-        <h1>
-          Promotions by category
-        </h1>
-        <h1>
-          View all
-        </h1>
+      <div
+        class="d-flex justify-space-between"
+        :class="{ 'mt-5': !isSmall, 'mt-n10': isSmall }"
+      >
+        <h1>Promotions by category</h1>
+        <h1>View all</h1>
       </div>
       <v-sheet
-        class="mx-auto mt-5"
+        class="mx-auto"
         elevation="0"
+        :class="{ 'mt-5': !isSmall, 'mt-n5': isSmall }"
       >
-        <v-slide-group
-          v-model="model2"
-          class="pa-4"
-        >
+        <v-slide-group v-model="model2" class="pa-4">
           <template #prev="{ on, attrs }">
-            <v-btn
-              color="white"
-              rounded
-              icon
-              v-bind="attrs"
-              v-on="on"
-            >
+            <v-btn color="white" rounded icon v-bind="attrs" v-on="on">
               <v-icon>mdi-arrow-left</v-icon>
             </v-btn>
           </template>
           <template #next="{ on, attrs }">
-            <v-btn
-              color="white"
-              rounded
-              icon
-              v-bind="attrs"
-              v-on="on"
-            >
+            <v-btn color="white" rounded icon v-bind="attrs" v-on="on">
               <v-icon>mdi-arrow-right</v-icon>
             </v-btn>
           </template>
@@ -106,6 +64,7 @@
             v-for="n in 15"
             :key="n"
             v-slot="{ toggle }"
+            class="mx-4"
           >
             <v-card
               :class="['ma-4 text-center']"
@@ -115,14 +74,25 @@
               style="border-radius: 12px; padding: 22px"
               @click="toggle"
             >
-              <div class="text-h6 " style="font-size: 16px;margin-bottom: 10px;line-height: 19.36px">
+              <div
+                class="text-h6"
+                style="
+                  font-size: 16px;
+                  margin-bottom: 10px;
+                  line-height: 19.36px;
+                "
+              >
                 Cakes
               </div>
-              <img
-                src="@/assets/images/cakes.png"
-                height="60"
+              <img src="@/assets/images/cakes.png" height="60" />
+              <div
+                style="
+                  font-weight: 600;
+                  font-size: 14px;
+                  margin-top: 10px;
+                  line-height: 16.94px;
+                "
               >
-              <div style="font-weight: 600; font-size: 14px; margin-top: 10px;line-height:16.94px">
                 <span class="text-red">32</span> Promos
               </div>
             </v-card>
@@ -131,18 +101,266 @@
       </v-sheet>
     </v-container>
   </div>
+  <OtherPromotion :items="otherPromotionData" />
 </template>
 
 <script>
+import OtherPromotion from "./OtherPromotion/OtherPromotion.vue";
+import otherImg from "@/assets/other-img-1.png";
+
 export default {
   name: "Promotions",
+  components: { OtherPromotion },
   data: () => ({
     model: null,
     model2: null,
+
+    screenWidth: window.innerWidth,
+    otherPromotionData: [
+      {
+        title: "By Meals",
+        items: [
+          {
+            title: "Chicken",
+            img: otherImg,
+            quantity: 2,
+          },
+          {
+            title: "Durian",
+            img: otherImg,
+            quantity: 2,
+          },
+          {
+            title: "Banana",
+            img: otherImg,
+            quantity: 2,
+          },
+          {
+            title: "Chicken",
+            img: otherImg,
+            quantity: 2,
+          },
+          {
+            title: "Durian",
+            img: otherImg,
+            quantity: 2,
+          },
+          {
+            title: "Banana",
+            img: otherImg,
+            quantity: 2,
+          },
+          {
+            title: "Chicken",
+            img: otherImg,
+            quantity: 2,
+          },
+          {
+            title: "Durian",
+            img: otherImg,
+            quantity: 2,
+          },
+          {
+            title: "Banana",
+            img: otherImg,
+            quantity: 2,
+          },
+        ],
+      },
+      {
+        title: "By People",
+        items: [
+          {
+            title: "Chicken",
+            img: otherImg,
+            quantity: 2,
+          },
+          {
+            title: "Durian",
+            img: otherImg,
+            quantity: 2,
+          },
+          {
+            title: "Banana",
+            img: otherImg,
+            quantity: 2,
+          },
+          {
+            title: "Chicken",
+            img: otherImg,
+            quantity: 2,
+          },
+          {
+            title: "Durian",
+            img: otherImg,
+            quantity: 2,
+          },
+          {
+            title: "Banana",
+            img: otherImg,
+            quantity: 2,
+          },
+        ],
+      },
+      {
+        title: "By Preferences",
+        items: [
+          {
+            title: "Chicken",
+            img: otherImg,
+            quantity: 2,
+          },
+          {
+            title: "Durian",
+            img: otherImg,
+            quantity: 2,
+          },
+          {
+            title: "Banana",
+            img: otherImg,
+            quantity: 2,
+          },
+          {
+            title: "Chicken",
+            img: otherImg,
+            quantity: 2,
+          },
+          {
+            title: "Durian",
+            img: otherImg,
+            quantity: 2,
+          },
+          {
+            title: "Banana",
+            img: otherImg,
+            quantity: 2,
+          },
+        ],
+      },
+      {
+        title: "By Meal",
+        items: [
+          {
+            title: "Chicken",
+            img: otherImg,
+            quantity: 2,
+          },
+          {
+            title: "Durian",
+            img: otherImg,
+            quantity: 2,
+          },
+          {
+            title: "Banana",
+            img: otherImg,
+            quantity: 2,
+          },
+          {
+            title: "Chicken",
+            img: otherImg,
+            quantity: 2,
+          },
+          {
+            title: "Durian",
+            img: otherImg,
+            quantity: 2,
+          },
+          {
+            title: "Banana",
+            img: otherImg,
+            quantity: 2,
+          },
+        ],
+      },
+      {
+        title: "By Meal",
+        items: [
+          {
+            title: "Chicken",
+            img: otherImg,
+            quantity: 2,
+          },
+          {
+            title: "Durian",
+            img: otherImg,
+            quantity: 2,
+          },
+          {
+            title: "Banana",
+            img: otherImg,
+            quantity: 2,
+          },
+          {
+            title: "Chicken",
+            img: otherImg,
+            quantity: 2,
+          },
+          {
+            title: "Durian",
+            img: otherImg,
+            quantity: 2,
+          },
+          {
+            title: "Banana",
+            img: otherImg,
+            quantity: 2,
+          },
+        ],
+      },
+      {
+        title: "By Meal",
+        items: [
+          {
+            title: "Chicken",
+            img: otherImg,
+            quantity: 2,
+          },
+          {
+            title: "Durian",
+            img: otherImg,
+            quantity: 2,
+          },
+          {
+            title: "Banana",
+            img: otherImg,
+            quantity: 2,
+          },
+          {
+            title: "Chicken",
+            img: otherImg,
+            quantity: 2,
+          },
+          {
+            title: "Durian",
+            img: otherImg,
+            quantity: 2,
+          },
+          {
+            title: "Banana",
+            img: otherImg,
+            quantity: 2,
+          },
+        ],
+      },
+    ],
   }),
-}
+  computed: {
+    isSmall() {
+      return this.screenWidth < 640;
+    },
+  },
+  created() {
+    window.addEventListener("resize", this.handleResize);
+  },
+  unmounted() {
+    window.removeEventListener("resize", this.handleResize);
+  },
+  methods: {
+    handleResize() {
+      this.screenWidth = window.innerWidth;
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
