6211553ccf0efd7549e2b650eb7bd9d67f507e48
ajiprasetyo471 <ajiprsty4713@gmail.com>
2023-05-05 10:21:56 +0700

    finish featured section

--- src/components/DesktopView/DesktopView.vue
+++ src/components/DesktopView/DesktopView.vue
@@ -2,6 +2,21 @@
   <Header />
   <Banner />
   <Promotions />
+  <Featured
+    title="Featured Malls"
+    desc="Check out promotions that are happening in malls around you"
+    :is-diff="false"
+  />
+  <Featured
+    title="Featured Merchants"
+    desc="Check out promotions that are happening with your Favorite Merchant"
+    :is-diff="false"
+  />
+  <Featured
+    title="Featured Merchants"
+    desc="Check out promotions that are happening with your Favorite Merchant"
+    :is-diff="true"
+  />
   <Footer />
 </template>
 
@@ -10,11 +25,13 @@ import Header from "@/components/Header.vue";
 import Banner from "@/components/Banner.vue";
 import Footer from "@/components/Footer.vue";
 import Promotions from "@/components/DesktopView/Promotions.vue";
+import Featured from "./Featured.vue";
 </script>
 
 <script>
 export default {
   name: "DesktopView",
+  components: { Featured },
   data() {
     return {
       drawer: false,
