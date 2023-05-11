5e6bd5140346f2ef83baa40fa1a02d2721644061
ajiprasetyo471 <ajiprsty4713@gmail.com>
2023-05-11 15:28:31 +0700

    change project folder

--- src/components/DesktopView/DesktopView.vue
+++ src/components/DesktopView/DesktopView.vue
@@ -26,8 +26,8 @@ import Header from "@/components/Header.vue";
 import Banner from "@/components/Banner.vue";
 import Footer from "@/components/Footer.vue";
 import Promotions from "@/components/DesktopView/Promotions/Promotions.vue";
-import Featured from "./Featured.vue";
-import Happening from "./Happening.vue";
+import Featured from "./Featured/Featured.vue";
+import Happening from "./Happening/Happening.vue";
 </script>
 
 <script>
