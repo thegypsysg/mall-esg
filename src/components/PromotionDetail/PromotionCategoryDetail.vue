65ecebf37158f25e53e2a29d567d1119e1087a16
ajiprasetyo471 <ajiprsty4713@gmail.com>
2023-05-16 20:50:33 +0700

    fix lazy load and skeleton view to promo category detail

--- src/components/PromotionDetail/PromotionCategoryDetail.vue
+++ src/components/PromotionDetail/PromotionCategoryDetail.vue
@@ -87,30 +87,54 @@
           </div>
         </div>
       </v-card> -->
-        <v-card
+        <div
           v-for="n in totalData"
           :key="n"
-          class="my-4 text-center"
-          :class="{ 'pa-2 mx-1': isSmall, ' mx-3': !isSmall }"
-          :elevation="isSmall ? 0 : 1"
-          :width="isSmall ? '22%' : '13%'"
-          style="border-radius: 12px; padding: 20px"
-          @click="toggle"
+          :class="{ 'card-item-2': isSmall, 'card-item': !isSmall }"
         >
-          <div
-            :class="{ 'fw-700': isSmall, 'text-h6': !isSmall }"
-            style="font-size: 16px; margin-bottom: 10px; line-height: 19.36px"
-          >
-            Cakes
-          </div>
-          <img src="@/assets/images/cakes.png" :height="isSmall ? 55 : 80" />
-          <div
-            style="font-weight: 600; margin-top: 10px; line-height: 16.94px"
-            :class="{ 'fs-10': isSmall, 'fs-14': !isSmall }"
-          >
-            <span class="text-red">32</span> Promos
-          </div>
-        </v-card>
+          <v-lazy :options="{ threshold: 0.5 }" min-height="100">
+            <v-card
+              class="my-4 text-center"
+              :class="{ 'pa-2 mx-1': isSmall, ' mx-3': !isSmall }"
+              :elevation="isSmall ? 0 : 1"
+              style="border-radius: 12px; padding: 20px"
+              @click="toggle"
+            >
+              <div
+                :class="{ 'fw-700': isSmall, 'text-h6': !isSmall }"
+                style="
+                  font-size: 16px;
+                  margin-bottom: 10px;
+                  line-height: 19.36px;
+                "
+              >
+                Cakes
+              </div>
+              <div
+                :class="{
+                  'card-img-container': !isSmall,
+                  'card-img-container-2': isSmall,
+                }"
+              >
+                <v-img
+                  src="@/assets/images/cakes.png"
+                  :height="isSmall ? 55 : 80"
+                  class="card-img"
+                >
+                  <template #placeholder>
+                    <div class="skeleton" />
+                  </template>
+                </v-img>
+              </div>
+              <div
+                style="font-weight: 600; margin-top: 10px; line-height: 16.94px"
+                :class="{ 'fs-10': isSmall, 'fs-14': !isSmall }"
+              >
+                <span class="text-red">32</span> Promos
+              </div>
+            </v-card>
+          </v-lazy>
+        </div>
       </div>
     </v-container>
     <Footer />
@@ -205,6 +229,32 @@ export default {
 .card-container {
   width: 100%;
 }
+.card-item {
+  width: 16% !important;
+}
+.card-item-2 {
+  width: 24% !important;
+}
+
+.card-img-container {
+  overflow: hidden;
+  width: 80px;
+  height: 80px;
+  margin: auto;
+}
+.card-img-container-2 {
+  overflow: hidden;
+  width: 55px;
+  height: 55px;
+  margin: auto;
+}
+
+.card-img {
+  object-fit: cover;
+  object-position: center;
+  width: 100%;
+  height: 100%;
+}
 
 .card-text {
   font-size: 14px;
@@ -219,5 +269,22 @@ export default {
 }
 .fs-14 {
   font-size: 14px;
+}
+
+.skeleton {
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(-90deg, #f2f2f2 0%, #e1e1e1 50%, #f2f2f2 100%);
+  background-size: 400% 400%;
+  animation: skeleton 1.6s ease infinite;
+}
+
+@keyframes skeleton {
+  0% {
+    background-position: 100% 0;
+  }
+  100% {
+    background-position: -100% 0;
+  }
 }
 </style>
