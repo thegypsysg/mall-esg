c054f05e6c61f0d96c1639ba63c44036213fdf26
aijprasetyo471 <ajiprsty4713@gmail.com>
2024-03-11 17:34:42 +0700

    fix featured malls desktop

--- src/components/DesktopView/Featured/Featured.vue
+++ src/components/DesktopView/Featured/Featured.vue
@@ -20,220 +20,234 @@
         {{ desc }}
       </h1>
       <v-sheet class="mx-n10 mt-5" elevation="0">
-        <div
-          class="py-2 px-6"
-        >
-        <div
-          v-if="isFeaturedMerchants"
-          class="d-flex flex-row flex-wrap"
-          :class="{
-            'justify-start': true,
-          }"
-        >        
-          <template 
-            v-for="(item, index) in mallMerchants"
-            :key="index"
-            class="mx-4"
+        <div class="py-2 px-6">
+          <div
+            v-if="isFeaturedMerchants"
+            class="d-flex flex-row flex-wrap"
+            :class="{
+              'justify-start': true,
+            }"
           >
-            <div class="">
-              <div
-                v-if="isDiff"
-                style="width: 85% !important; gap: 5px"
-                class="card-info ml-3 d-flex flex-column"
-              >
-                <p style="font-weight: 600; font-size: 16px">
-                  Hainan Chicken Rice with steamed Chicken topped with Soya
-                  Sauce
-                </p>
-                <div style="font-weight: 600; font-size: 14px">
-                  32 <span class="text-muted">Purchased</span> 50
-                  <span class="text-muted">Views</span>
-                </div>
-              </div>
-              <v-lazy :options="{ threshold: 0.5 }" min-height="370">
-                <v-card
-                  class="my-4 mx-3 featured-card"
-                  width="370"
-                  elevation="1"
-                  @click="toggle"
-                >
-
+            <template
+              v-for="(item, index) in mallMerchants"
+              :key="index"
+              class="mx-4"
+            >
+              <div class="">
                 <div
-                  v-if="!isDiff"
-                  class="card-title-container d-flex justify-space-between align-center px-2 py-4"
+                  v-if="isDiff"
+                  style="width: 85% !important; gap: 5px"
+                  class="card-info ml-3 d-flex flex-column"
                 >
-                  <div class="w-25 pr-3">
-                    <v-img :src="this.$fileURL + item?.partner?.logo" height="30" />
-                  </div>
-                  <div class="w-75 d-flex align-center justify-space-between">
-                    <div class="card-title">
-                      <h4 class="text-no-wrap">{{ item?.partner?.partner_name }}</h4>
-                      <span class="text-no-wrap text-blue">{{ item?.location_name }}, {{ item?.unit_number }}</span>
-                      <br />
-                      <span class="text-no-wrap text-primary">{{ item?.town?.town_name }}</span>
-                    </div>
-                    <span class="text-red card-title-right text-no-wrap text-red">{{ item?.distance }}</span>
+                  <p style="font-weight: 600; font-size: 16px">
+                    Hainan Chicken Rice with steamed Chicken topped with Soya
+                    Sauce
+                  </p>
+                  <div style="font-weight: 600; font-size: 14px">
+                    32 <span class="text-muted">Purchased</span> 50
+                    <span class="text-muted">Views</span>
                   </div>
                 </div>
-                  <div class="featured-card-img-cont">
-                    <v-img
-                      class="featured-card-img"
-                      :src="this.$fileURL + item?.location_image"
-                      transition="fade-transition"
+                <v-lazy :options="{ threshold: 0.5 }" min-height="370">
+                  <v-card
+                    class="my-4 mx-3 featured-card"
+                    width="370"
+                    elevation="1"
+                    @click="toggle"
+                  >
+                    <div
+                      v-if="!isDiff"
+                      class="card-title-container d-flex justify-space-between align-center px-2 py-4"
                     >
-                      <template #placeholder>
-                        <div class="skeleton" />
-                      </template>
-                    </v-img>
-                    <!-- <div class="skeleton" /> -->
-                  </div>
-                  <div
-                    v-if="!isDiff"
-                    style="
-                      font-weight: 400;
-                      font-size: 14px;
-                      position: relative;
-                    "
-                    class="px-2 pt-5 pb-2"
-                  >
-                    15 Merchants | 12 Promotions
-                  </div>
-
-
-
-                  <div
-                    v-if="isDiff"
-                    class="card-description pa-2 d-flex flex-column"
-                    style="position: relative; gap: 10px"
-                  >
-                    <div class="card-rating">
-                      <v-icon color="#F63F17"> mdi-star </v-icon>
-                      <v-icon color="#F63F17"> mdi-star </v-icon>
-                      <v-icon color="#F63F17"> mdi-star </v-icon>
-                      <v-icon color="#F63F17"> mdi-star </v-icon>
-                      <v-icon color="#F63F17"> mdi-star-outline </v-icon>
-                      <span class="ml-2">( 132 rates )</span>
-                    </div>
-                    <div class="card-price">
-                      <img src="@/assets/featured-price-img.png" height="15" />
-                      <span class="ml-5">$80</span>
-                    </div>
-                    <div class="card-time">
-                      <v-icon color="#ABABAB"> mdi-clock-outline </v-icon
-                      ><span class="ml-4" style="color: #ababab"
-                        >Ends in 18 Days (15/03/2023)</span
+                      <div class="w-25 pr-3">
+                        <v-img
+                          :src="$fileURL + item?.partner?.logo"
+                          height="30"
+                        />
+                      </div>
+                      <div
+                        class="w-75 d-flex align-center justify-space-between"
                       >
+                        <div class="card-title">
+                          <h4 class="text-no-wrap">
+                            {{ item?.partner?.partner_name }}
+                          </h4>
+                          <span class="text-no-wrap text-caption text-blue"
+                            >{{ item?.location_name }},
+                            {{ item?.unit_number }}</span
+                          >
+                          <br />
+                          <span class="text-no-wrap text-primary">{{
+                            item?.town?.town_name
+                          }}</span>
+                        </div>
+                        <span
+                          class="text-red card-title-right text-no-wrap text-red"
+                          >{{ item?.distance }}</span
+                        >
+                      </div>
+                    </div>
+                    <div class="featured-card-img-cont">
+                      <v-img
+                        class="featured-card-img"
+                        :src="$fileURL + item?.location_image"
+                        transition="fade-transition"
+                      >
+                        <template #placeholder>
+                          <div class="skeleton" />
+                        </template>
+                      </v-img>
+                      <!-- <div class="skeleton" /> -->
                     </div>
                     <div
-                      class="card-address d-flex align-center"
-                      style="gap: 25px"
+                      v-if="!isDiff"
+                      style="
+                        font-weight: 400;
+                        font-size: 14px;
+                        position: relative;
+                      "
+                      class="px-2 pt-5 pb-2"
                     >
-                      <img
-                        src="@/assets/featured-address-img.png"
-                        width="24"
-                        height="28"
-                      />
-                      <div class="card-address-info">
-                        <h4>Papa Rich</h4>
-                        <h4>Parkway Parade, #01-23</h4>
-                        <h4>Marine Parade</h4>
+                      15 Merchants | 12 Promotions
+                    </div>
+
+                    <div
+                      v-if="isDiff"
+                      class="card-description pa-2 d-flex flex-column"
+                      style="position: relative; gap: 10px"
+                    >
+                      <div class="card-rating">
+                        <v-icon color="#F63F17"> mdi-star </v-icon>
+                        <v-icon color="#F63F17"> mdi-star </v-icon>
+                        <v-icon color="#F63F17"> mdi-star </v-icon>
+                        <v-icon color="#F63F17"> mdi-star </v-icon>
+                        <v-icon color="#F63F17"> mdi-star-outline </v-icon>
+                        <span class="ml-2">( 132 rates )</span>
+                      </div>
+                      <div class="card-price">
+                        <img
+                          src="@/assets/featured-price-img.png"
+                          height="15"
+                        />
+                        <span class="ml-5">$80</span>
+                      </div>
+                      <div class="card-time">
+                        <v-icon color="#ABABAB"> mdi-clock-outline </v-icon
+                        ><span class="ml-4" style="color: #ababab"
+                          >Ends in 18 Days (15/03/2023)</span
+                        >
+                      </div>
+                      <div
+                        class="card-address d-flex align-center"
+                        style="gap: 25px"
+                      >
+                        <img
+                          src="@/assets/featured-address-img.png"
+                          width="24"
+                          height="28"
+                        />
+                        <div class="card-address-info">
+                          <h4>Papa Rich</h4>
+                          <h4>Parkway Parade, #01-23</h4>
+                          <h4>Marine Parade</h4>
+                        </div>
+                      </div>
+                      <div
+                        class="card-offer py-5 d-flex align-center"
+                        style="background: #f3f3f3; gap: 20px; color: #5e5e5e"
+                      >
+                        <img src="@/assets/featured-offer-img.png" />
+                        <span>Flat 50% on all Stores</span>
                       </div>
                     </div>
                     <div
-                      class="card-offer py-5 d-flex align-center"
-                      style="background: #f3f3f3; gap: 20px; color: #5e5e5e"
+                      class="card-btn-container d-flex justify-space-between"
+                      :class="{
+                        'card-btn-container-1': !isDiff,
+                        'card-btn-container-2': isDiff,
+                      }"
                     >
-                      <img src="@/assets/featured-offer-img.png" />
-                      <span>Flat 50% on all Stores</span>
+                      <v-btn
+                        variant="outlined"
+                        color="black"
+                        class="card-btn"
+                        width="32"
+                        height="32"
+                        icon="mdi-share-variant-outline"
+                      >
+                        <v-icon color="rgb(38, 38, 38)" size="22">
+                          mdi-share-variant-outline
+                        </v-icon>
+                        <v-menu activator="parent">
+                          <v-list>
+                            <v-list-item @click="console.log('share')">
+                              <v-list-item-title>
+                                <v-icon class="mr-4" color="black" size="18">
+                                  mdi-email-outline </v-icon
+                                >Email
+                              </v-list-item-title>
+                            </v-list-item>
+                            <v-list-item @click="console.log('share')">
+                              <v-list-item-title>
+                                <v-icon class="mr-4" size="18">
+                                  <i class="fa-brands fa-facebook-f" /> </v-icon
+                                >Facebook
+                              </v-list-item-title>
+                            </v-list-item>
+                            <v-list-item @click="console.log('share')">
+                              <v-list-item-title>
+                                <v-icon class="mr-4" color="black" size="18">
+                                  mdi-twitter </v-icon
+                                >Twitter
+                              </v-list-item-title>
+                            </v-list-item>
+                            <v-list-item @click="console.log('share')">
+                              <v-list-item-title>
+                                <v-icon class="mr-4" size="18">
+                                  <i
+                                    class="fa-brands fa-linkedin-in"
+                                  /> </v-icon
+                                >Linkedin
+                              </v-list-item-title>
+                            </v-list-item>
+                          </v-list>
+                        </v-menu>
+                      </v-btn>
+                      <v-btn
+                        class="card-btn"
+                        variant="outlined"
+                        color="black"
+                        icon="mdi-heart"
+                        width="32"
+                        height="32"
+                      >
+                        <v-icon color="red"> mdi-heart </v-icon>
+                      </v-btn>
                     </div>
-                  </div>
-                  <div
-                    class="card-btn-container d-flex justify-space-between"
-                    :class="{
-                      'card-btn-container-1': !isDiff,
-                      'card-btn-container-2': isDiff,
-                    }"
-                  >
-                    <v-btn
-                      variant="outlined"
-                      color="black"
-                      class="card-btn"
-                      width="32"
-                      height="32"
-                      icon="mdi-share-variant-outline"
-                    >
-                      <v-icon color="rgb(38, 38, 38)" size="22">
-                        mdi-share-variant-outline
-                      </v-icon>
-                      <v-menu activator="parent">
-                        <v-list>
-                          <v-list-item @click="console.log('share')">
-                            <v-list-item-title>
-                              <v-icon class="mr-4" color="black" size="18">
-                                mdi-email-outline </v-icon
-                              >Email
-                            </v-list-item-title>
-                          </v-list-item>
-                          <v-list-item @click="console.log('share')">
-                            <v-list-item-title>
-                              <v-icon class="mr-4" size="18">
-                                <i class="fa-brands fa-facebook-f" /> </v-icon
-                              >Facebook
-                            </v-list-item-title>
-                          </v-list-item>
-                          <v-list-item @click="console.log('share')">
-                            <v-list-item-title>
-                              <v-icon class="mr-4" color="black" size="18">
-                                mdi-twitter </v-icon
-                              >Twitter
-                            </v-list-item-title>
-                          </v-list-item>
-                          <v-list-item @click="console.log('share')">
-                            <v-list-item-title>
-                              <v-icon class="mr-4" size="18">
-                                <i class="fa-brands fa-linkedin-in" /> </v-icon
-                              >Linkedin
-                            </v-list-item-title>
-                          </v-list-item>
-                        </v-list>
-                      </v-menu>
-                    </v-btn>
-                    <v-btn
-                      class="card-btn"
-                      variant="outlined"
-                      color="black"
-                      icon="mdi-heart"
-                      width="32"
-                      height="32"
-                    >
-                      <v-icon color="red"> mdi-heart </v-icon>
-                    </v-btn>
-                  </div>
 
-                  <div
-                    v-if="item?.mall_merchant?.featured === 'Y'"
-                    class="card-tag"
-                    :class="{
-                      'card-tag-1': isDiff,
-                      'card-tag-2': !isDiff,
-                    }"
-                    style="top:120px"
-                  >
-                    Featured
-                  </div>
-                  <div v-if="isDiff" class="card-distance">
-                    <v-icon color="#808080"> mdi-map-marker </v-icon>
-                    <span class="text-red">10.20 kms</span
-                    ><span class="text-muted"> away</span>
-                  </div>
-                </v-card>
-              </v-lazy>
-            </div>
-          </template>
+                    <div
+                      v-if="item?.mall_merchant?.featured === 'Y'"
+                      class="card-tag"
+                      :class="{
+                        'card-tag-1': isDiff,
+                        'card-tag-2': !isDiff,
+                      }"
+                      style="top: 120px"
+                    >
+                      Featured
+                    </div>
+                    <div v-if="isDiff" class="card-distance">
+                      <v-icon color="#808080"> mdi-map-marker </v-icon>
+                      <span class="text-red">10.20 kms</span
+                      ><span class="text-muted"> away</span>
+                    </div>
+                  </v-card>
+                </v-lazy>
+              </div>
+            </template>
           </div>
         </div>
 
-        
         <div
           v-if="isSlide == false"
           class="d-flex flex-row flex-wrap"
@@ -456,7 +470,7 @@ export default {
     "activeMallCards",
     "activeMerchantItems",
     "mallMerchants",
-    "isFeaturedMerchants"
+    "isFeaturedMerchants",
   ],
   data() {
     return {
